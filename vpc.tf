e27c56fa4cf1fef427c3cfa215b56e0b1fa961ff
wippet-boy <john-r.grant@virginmedia.com>
2021-05-18 11:26:07 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/bigbadbobbyg-training/network/google"
-  version = "2.5.0"
+  version = "3.2.0"
   network_name = "gaurav-network"
   project_id = var.project
 
